e5eb580c769cce1cb43840ee4b6a284d478b0d63
rdwebdev24 <rohitdhakad101@gmail.com>
2023-03-08 20:22:18 +0530

    loadsh error resolve

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const fetch1 = require('./fetch')
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-const { sum } = require('lodash');
 
 app.use(cors());
 app.use(bodyParser.json())
